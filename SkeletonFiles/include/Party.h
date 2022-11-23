f642c763a1c17c3208369f78e66de5b67f4dd7e5
GuyG1996 <guyginat0@gmail.com>
2022-11-23 17:22:01 +0200

    Add files via upload

---
+++ SkeletonFiles/include/Party.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <string>
+
+using std::string;
+
+class JoinPolicy;
+class Simulation;
+
+enum State
+{
+    Waiting,
+    CollectingOffers,
+    Joined
+};
+
+class Party
+{
+public:
+    Party(int id, string name, int mandates, JoinPolicy *); 
+    virtual ~Party();
+    Party(const Party &other);
+    Party& operator=(const Party& other);
+    Party::Party(Party&& other);
+    Party& operator=(Party&& other);
+
+
+    State getState() const;
+    void setState(State state);
+    int getMandates() const;
+    void step(Simulation &s);
+    const string &getName() const;
+
+private:
+    int mId;
+    string mName;
+    int mMandates;
+    JoinPolicy *mJoinPolicy;
+    State mState;
+};
